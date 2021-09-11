0acf0e945d5aea5fc8ad6dfe2a59ca312ccd67ce
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-09-11 16:46:20 -0500

    Added VideoPlayer

--- src/views/VideoPlayer.vue
+++ src/views/VideoPlayer.vue
@@ -1,12 +1,37 @@
 <template>
-  <v-row>
-
-  </v-row>
+  <div>
+    <video ref="videoPlayer" class="video-js">
+      <source :src="`https://stream.raajpatel.dev/${store.state.streamkey['playback_ids'][0]['id']}.m3u8`" type="application/x-mpegURL">
+    </video>
+  </div>
 </template>
 
 <script>
+import videojs from 'video.js';
+// import 'videojs/dist/video-js.css';
+
 export default {
-  name: "VideoPlayer"
+  name: "VideoPlayer",
+  data() {
+    return {
+      player: null
+    }
+  },
+  mounted(){
+    this.player = videojs(this.$refs.videoPlayer, {autoplay: 'muted', controls: true, fluid: true}, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
+  },
+  created(){
+    if(!this.$store.state.login){
+      this.$router.push("/");
+    }
+  },
+  beforeDestroy() {
+    if(this.player){
+      this.player.dispose();
+    }
+  }
 }
 </script>
 
