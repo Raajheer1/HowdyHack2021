828696a8661df221b31c6425f847e18b75ce0855
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-09-11 14:09:00 -0500

    Vuex Config + Vuex-Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    appURL: "https://howdy.raajpatel.dev/",
+    login: false,
+    drawer: null,
+    assets: null,
+    title: null,
+    streamkey: null,
+    internalvideo: null,
+    internalasset: null,
+    streams: null,
+    loading: true,
+    sheet: false,
   },
   mutations: {
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
